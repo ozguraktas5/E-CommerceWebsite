48bced7473c0e0afbf4a4f4df16360a575953dcf
Özgür Aktaş <ozguraktas.55555@gmail.com>
2023-06-28 17:35:54 +0300

    v1

---
+++ script.js
@@ -0,0 +1,31 @@
+// copy menu for mobile
+function copyMenu() {
+    // copy inside .dpt-cat to .departments
+    var dptCategory = document.querySelector('.dpt-cat');
+    var dptPlace = document.querySelector('.departments');
+    dptPlace.innerHTML = dptCategory.innerHTML;
+
+    // copy inside nav to nav
+    var mainNav = document.querySelector('.header-nav nav');
+    var navPlace = document.querySelector('.off-canvas nav');
+    navPlace.innerHTML = mainNav.innerHTML;
+
+    // copy .header-top .wrapper to .thetop-nav
+    var topNav = document.querySelector('.header-top .wrapper');
+    var topPlace = document.querySelector('.off-canvas .thetop-nav');
+    topPlace.innerHTML = topNav.innerHTML;
+
+}
+
+copyMenu();
+
+//show sub menu on mobile
+const submenu = document.querySelectorAll('.has-child .icon-small');
+submenu.forEach((menu) => menu.addEventListener('click', toggle));
+
+function toggle(e) {
+    e.preventDefault();
+    submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand') : null);
+    if (this.closest('.has-child').classList != 'expand');
+    this.closest('.has-child').classList.toggle('expand');
+}
