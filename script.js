3cf3b501eec5f02367246e4e38d9a74ebb061755
Özgür Aktaş <ozguraktas.55555@gmail.com>
2023-07-10 09:20:06 +0300

    v1

--- script.js
+++ script.js
@@ -50,8 +50,17 @@ const swiper = new Swiper(".swiper", {
   pagination: {
     el: ".swiper-pagination",
   },
-
- 
-
-  
 });
+
+//show search
+
+const searchButton = document.querySelector('.t-search'),
+  tClose = document.querySelector('.search-close'),
+  showClass = document.querySelector('.site');
+
+searchButton.addEventListener('click', function() {
+  showClass.classList.toggle('showsearch')
+})
+tClose.addEventListener('click', function() {
+  showClass.classList.remove('showsearch')
+})
